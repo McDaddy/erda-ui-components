5fcdd711128ec13fd5fcd6c2bfabd41ac01a9662
陈韡韬 <mcdaddychen@126.com>
2021-12-01 20:04:27 +0800

    add dumi

---
+++ .umirc.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'dumi';
+
+const title = 'Erda UI Components';
+const repo = 'components';
+
+export default defineConfig({
+  title,
+  favicon:
+    '/images/favicon.png',
+  logo:
+    '/images/favicon.png',
+  outputPath: 'docs-dist',
+  mode: 'site',
+  hash: true,
+  // Because of using GitHub Pages
+  base: `/${repo}/`,
+  publicPath: `/${repo}/`,
+  navs: [
+    null,
+    {
+      title: 'GitHub',
+      path: 'https://github.com/umijs/dumi-template',
+    },
+  ],
+  // more config: https://d.umijs.org/config
+});
